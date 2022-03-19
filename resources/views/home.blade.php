8738e7de0432e88650e0e6ddb8a246b43b35734c
HerrAlpha <magungrizki@icloud.com>
2022-03-19 14:06:31 +0700

    update navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,12 +17,43 @@
         font-family: -apple-system, BlinkMacSystemFont, sans-serif;
         font-weight: semibold;
     }
+    #1{
+        font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+        font-weight: bold;
+    }
     .navbar-nav{
         alignment: center;
     }
     </style>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" id="1" href="/">UKlise</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/templates">Templates</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/web">Web</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/app">Apps</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     <h2>UKlise</h2>
     <h4>First Laravel and Flutter Template Today</h4>
 </body>
