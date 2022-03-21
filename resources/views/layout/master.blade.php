fdfbbcd77c62e72fc492488ebb0214c9d8e7a6f2
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2022-03-21 16:08:04 +0700

    Navbar dikanan

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -45,7 +45,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav" style="text-align: right;">
+          <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav" style="text-align: right;">
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="/">Home</a>
