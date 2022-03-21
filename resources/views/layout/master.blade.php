7e2dfb4487d6839537fa11d61fa9bc31ad737350
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2022-03-21 22:24:45 +0700

    font-size navbar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -59,11 +59,11 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <div class="container-fluid">
-          <a class="navbar-brand" id="1" href="/">UKlise</a>
+          <a class="navbar-brand fs-3" id="1" href="/">UKlise</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav" style="text-align: right;">
+          <div class="collapse navbar-collapse flex-row-reverse fs-4" id="navbarNav" style="text-align: right;">
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
