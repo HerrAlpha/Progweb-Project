865b9a478c1b834a2d252088563ddc62445f29bb
HerrAlpha <magungrizki@icloud.com>
2022-03-22 14:08:16 +0700

    Update master.blade.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -57,13 +57,13 @@
 
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="padding:0.5%;">
         <div class="container-fluid">
           <a class="navbar-brand fs-3" id="1" href="/">UKlise</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse flex-row-reverse fs-4" id="navbarNav" style="text-align: right;">
+          <div class="collapse navbar-collapse flex-row-reverse "  style="text-align: right;">
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
