984e19fb3f8ac9ebc515b8fc57123ee3d969d5d2
HerrAlpha <magungrizki@icloud.com>
2022-04-29 20:51:06 +0700

    update news #1

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,47 +1,28 @@
 @extends('layout.master')
 @section('title','Tech News')
-@section('css','web')
-@section('js','')
 @section('bg-body','bg-dark')
-@section('class-navbar','bg-primary')
+{{-- @section('class-navbar','bg-primary') --}}
 @section('content')
 
-<div class="container mt-5">
-
-    @for ($i = 0; $i<10;$i++)
-    <div class="row mb-5">
-        <div class="card mb-3">
-            <img src="{{$news[$i]->urlToImage}}" class="card-img-top" height="500">
-            <div class="card-body">
-                <div class="row text-center">
-                    <div class="col-lg-12">
-                        <h2>{{$news[$i]->title}}</h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <p class="card-text">{{$news[$i]->description}}</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-3">
-                       Author : {{$news[$i]->author}}
-                    </div>
-                    <div class="col-lg-3">
-                        By : {{$news[$i]->source->name}}
-                    </div>
-                    <div class="col-lg-3">
-                        <a class="link-primary" href="{{$news[$i]->url}}">More Information</a>
-                    </div>
-                    <div class="col-lg-3">
-                        <p class="card-text"><small class="text-muted">{{$news[$i]->publishedAt}}</small></p>
-                    </div>
+<div class="container" style="padding:3%">
+<h3 style="text-align: center; color: #f2f2f7">Programming Technology</h3>
+<div class="row g-3">
+    @foreach ($news as $news)
+            <div class="col-md-3" style="padding:1%">
+                <div class="card" style="width: 100px; height:300px">
+                        <img src="{{$news->urlToImage}}" alt="" class="card-img-top">
+                        <div class="card-header">
+                            {{$news->title}}
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">Author : {{$news->author}}</p>
+                            <p class="card-text">Source : {{$news->source->name}}</p>
+                            <a class="link-primary" href="{{$news->url}}">Read More</a>
+                            <p class="card-text"><small class="text-muted">Post at : {{$news->publishedAt}}</small></p>
                 </div>
             </div>
-          </div>
-    </div>
-    @endfor
-
+    @endforeach
+ 
 </div>
-
+</div>
 @stop
