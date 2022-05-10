d2025c46919d48db2873788bc0d36b74c3964b88
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2022-05-10 07:45:45 +0700

    Update web-category.blade.php

--- resources/views/web/web-category.blade.php
+++ resources/views/web/web-category.blade.php
@@ -1,29 +1,9 @@
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <title></title>
-</head>
-<body class='bg-light   '>
-<h1 class="text-center my-5">{{$web->title}}</h1>
-<hr class="border-bottom border-5 border-dark">
-{{-- @dump($post) --}}
-@foreach ($post as $post)
-    {{$post->title}}
-@endforeach
-</body>
-</html>
-=======
 @extends('layout.master')
 @section('content')
 <div class="container">
-    @foreach ($categories as $category)
-    <title>{{$category->title}}</title>
+    <h1 class="text-center">{{$web->title}}</h1>
+    @foreach ($post as $post)
+    <title>{{$post->title}}</title>
     @endforeach
 
 @stop
->>>>>>> Stashed changes
