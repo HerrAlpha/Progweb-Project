ea27711a72bf139493bb6f54d935310a9b75d9a4
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2022-05-23 07:44:46 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,165 +27,182 @@ class DatabaseSeeder extends Seeder
         //----------------------------------SECTION USER--------------------------------------------
 
         User::create([
-            'name'=> 'Melissa J Tracy',
+            'name' => 'Melissa J Tracy',
             'username' => 'melissa',
             'email' => 'melisT@gmail.com',
             'password' => bcrypt('password'),
             'phone' => '212-608-4689',
-            'address'=> '3701 Wurm Rd Wolverine, Michigan(MI), 49799',
-            'city'=>'Michigan',
-            'state'=>'Indonesia',
-            'zip'=>'47231',
-            'country'=>'Indiana',
-            'company'=> 'Road Runner Lawn Services',
-            'avatar'=> 'avatar/seed1.jpg',
-            'web'=>'1',
-            'app'=>'',
-            'about'=>'Pengen Nafas Manual'
+            'address' => '3701 Wurm Rd Wolverine, Michigan(MI), 49799',
+            'city' => 'Michigan',
+            'state' => 'Indonesia',
+            'zip' => '47231',
+            'country' => 'Indiana',
+            'company' => 'Road Runner Lawn Services',
+            'avatar' => 'avatar/seed1.jpg',
+            'web' => '1',
+            'app' => '',
+            'about' => 'Pengen Nafas Manual'
         ]);
 
         User::create([
-            'name'=> 'Aurum Team',
+            'name' => 'Aurum Team',
             'username' => 'adminadmin',
             'email' => 'aherr249@gmail.com',
             'password' => bcrypt('password'),
             'phone' => '260-316-6573',
-            'address'=> 'Jalan Semarang 5 Malang Kec. Lowokwaru - Kota Malang - Prov. Jawa Timur',
-            'city'=>'Malang',
-            'state'=>'Indonesia',
-            'zip'=>'65145',
-            'country'=>'Indonesia',
-            'company'=> 'Universitas Negeri Malang',
-            'avatar'=> 'avatar/seed2.jpg',
-            'web'=>'',
-            'app'=>'1',
-            'about'=>'Full SenyUM'
+            'address' => 'Jalan Semarang 5 Malang Kec. Lowokwaru - Kota Malang - Prov. Jawa Timur',
+            'city' => 'Malang',
+            'state' => 'Indonesia',
+            'zip' => '65145',
+            'country' => 'Indonesia',
+            'company' => 'Universitas Negeri Malang',
+            'avatar' => 'avatar/seed2.jpg',
+            'web' => '',
+            'app' => '1',
+            'about' => 'Full SenyUM'
         ]);
 
         User::create([
-            'name'=> 'Carol K Rochelle',
+            'name' => 'Carol K Rochelle',
             'username' => 'carol',
             'email' => 'carolRoc@gmail.com',
             'password' => bcrypt('password'),
             'phone' => '508-230-9846',
-            'address'=> '4281 Stadium Drive',
-            'city'=>'Easton',
-            'state'=>'Massachusetts',
-            'zip'=>'02334',
-            'country'=>'Massachusetts',
-            'company'=> 'VitaGrey',
-            'avatar'=> 'avatar/seed3.jpg',
-            'web'=>'1',
-            'app'=>'',
-            'about'=>'Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run Import-Module PSReadLine'
+            'address' => '4281 Stadium Drive',
+            'city' => 'Easton',
+            'state' => 'Massachusetts',
+            'zip' => '02334',
+            'country' => 'Massachusetts',
+            'company' => 'VitaGrey',
+            'avatar' => 'avatar/seed3.jpg',
+            'web' => '1',
+            'app' => '',
+            'about' => 'Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run Import-Module PSReadLine'
         ]);
 
         User::create([
-            'name'=> 'Lauren R Williams',
+            'name' => 'Lauren R Williams',
             'username' => 'lauren',
             'email' => 'laurenrr@gmail.com',
             'password' => bcrypt('password'),
             'phone' => '270-828-9431',
-            'address'=> '1192 Crosswind Drive',
-            'city'=>'North Garrett',
-            'state'=>'Kentucky',
-            'zip'=>'123456',
-            'country'=>'Kentucky',
-            'company'=> 'Thorofare',
-            'avatar'=> 'avatar/seed4.jpg',
-            'web'=>'1',
-            'app'=>'',
-            'about'=>'Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run Import-Module PSReadLine'
+            'address' => '1192 Crosswind Drive',
+            'city' => 'North Garrett',
+            'state' => 'Kentucky',
+            'zip' => '123456',
+            'country' => 'Kentucky',
+            'company' => 'Thorofare',
+            'avatar' => 'avatar/seed4.jpg',
+            'web' => '1',
+            'app' => '',
+            'about' => 'Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run Import-Module PSReadLine'
         ]);
 
         User::create([
-            'name'=> 'Thelma J Mendoza',
+            'name' => 'Thelma J Mendoza',
             'username' => 'thelma',
             'email' => 'thelmajme@gmail.com',
             'password' => bcrypt('password'),
             'phone' => '956-202-2896',
-            'address'=> '2183 Jerome Avenue',
-            'city'=>'Dallas',
-            'state'=>'Texas',
-            'zip'=>'75252',
-            'country'=>'Texas',
-            'company'=> 'Universitas Negeri Malang',
-            'avatar'=> 'avatar/seed5.jpg',
-            'web'=>'1',
-            'app'=>'',
-            'about'=>'Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run Import-Module PSReadLine'
+            'address' => '2183 Jerome Avenue',
+            'city' => 'Dallas',
+            'state' => 'Texas',
+            'zip' => '75252',
+            'country' => 'Texas',
+            'company' => 'Universitas Negeri Malang',
+            'avatar' => 'avatar/seed5.jpg',
+            'web' => '1',
+            'app' => '',
+            'about' => 'Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run Import-Module PSReadLine'
+        ]);
+
+        User::create([
+            'name' => 'M Baharuddin Yusuf',
+            'username' => 'nguyab',
+            'email' => 'muhammadbaharuddin08123@gmail.com',
+            'password' => bcrypt('password'),
+            'phone' => '956-202-2896',
+            'address' => '2183 Jerome Avenue',
+            'city' => 'Dallas',
+            'state' => 'Texas',
+            'zip' => '75252',
+            'country' => 'Texas',
+            'company' => 'Universitas Negeri Malang',
+            'avatar' => 'avatar/seed5.jpg',
+            'web' => '1',
+            'app' => '',
+            'about' => 'Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run Import-Module PSReadLine'
         ]);
 
         //--------------------------------SECTION CATEGORY---------------------------------
 
         Category::create([
-            'title'=>'Sport',
-            'cover'=>'/assets/picture/app/category/sport.png'
+            'title' => 'Sport',
+            'cover' => '/assets/picture/app/category/sport.png'
         ]);
 
         Category::create([
-            'title'=>'Technology',
-            'cover'=>'/assets/picture/app/category/technology.png'
+            'title' => 'Technology',
+            'cover' => '/assets/picture/app/category/technology.png'
         ]);
 
         Category::create([
-            'title'=>'Business',
-            'cover'=>'/assets/picture/app/category/business.png'
+            'title' => 'Business',
+            'cover' => '/assets/picture/app/category/business.png'
         ]);
 
         Category::create([
-            'title'=>'Entertainment',
-            'cover'=>'/assets/picture/app/category/entertainment.png'
+            'title' => 'Entertainment',
+            'cover' => '/assets/picture/app/category/entertainment.png'
         ]);
 
         Category::create([
-            'title'=>'Health',
-            'cover'=>'/assets/picture/app/category/health.png'
+            'title' => 'Health',
+            'cover' => '/assets/picture/app/category/health.png'
         ]);
 
         Category::create([
-            'title'=>'Science',
-            'cover'=>'/assets/picture/app/category/science.png'
+            'title' => 'Science',
+            'cover' => '/assets/picture/app/category/science.png'
         ]);
 
         Category::create([
-            'title'=>'Travel',
-            'cover'=>'/assets/picture/app/category/travel.png'
+            'title' => 'Travel',
+            'cover' => '/assets/picture/app/category/travel.png'
         ]);
 
         Category::create([
-            'title'=>'Food',
-            'cover'=>'/assets/picture/app/category/food.png'
+            'title' => 'Food',
+            'cover' => '/assets/picture/app/category/food.png'
         ]);
 
         Category::create([
-            'title'=>'Finance',
-            'cover'=>'/assets/picture/app/category/finance.png'
+            'title' => 'Finance',
+            'cover' => '/assets/picture/app/category/finance.png'
         ]);
 
         Category::create([
-            'title'=>'News',
-            'cover'=>'/assets/picture/app/category/news.png'
+            'title' => 'News',
+            'cover' => '/assets/picture/app/category/news.png'
         ]);
 
         Category::create([
-            'title'=>'Music',
-            'cover'=>'/assets/picture/app/category/music.png'
+            'title' => 'Music',
+            'cover' => '/assets/picture/app/category/music.png'
         ]);
 
         //-----------------------------SECTION POSTS-----------------------------------------------
 
         Post::create([
-            'title'=>'Natural Vacation',
-            'description'=>'For who interest to make a website with theme Travel, we provide this for you',
-            'category_id'=>7,
-            'user_id'=>1,
-            'framework'=>'laravel',
-            'compatibility'=>'Mobile App',
-            'file'=> 'file/contoh.jpg',
-            'cover'=>'cover/contoh.jpg',
+            'title' => 'Natural Vacation',
+            'description' => 'For who interest to make a website with theme Travel, we provide this for you',
+            'category_id' => 7,
+            'user_id' => 1,
+            'framework' => 'laravel',
+            'compatibility' => 'Mobile App',
+            'file' => 'file/contoh.jpg',
+            'cover' => 'cover/contoh.jpg',
             'price' => '2',
         ]);
-
     }
 }
