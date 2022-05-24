85dadbff2f682185236d8cb215354d2f832eda77
HerrAlpha <magungrizki@icloud.com>
2022-05-24 13:19:20 +0700

    add admin

---
+++ database/migrations/2022_05_24_043136_create_admin_uklise.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_uklise', function (Blueprint $table) {
+            $table->id('id_admin');
+            $table->string('username');
+            $table->string('password');
+            $table->integer('code_authorization');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_uklise');
+    }
+};
