42a70a90e385d176584fd89eb39d181e50e94671
HerrAlpha <magungrizki@icloud.com>
2022-05-30 18:45:24 +0700

    Update forgetpassword.blade.php

--- resources/views/mail/forgetpassword.blade.php
+++ resources/views/mail/forgetpassword.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="/css/bootstrap.grid.css">
 <link rel="stylesheet" href="/js/bootstrap.min.js">
 
-
+<img src="/assets/picture/Logo-nonText.png" style="text-align: center; width: 400px; height: 400px" alt="">
 <h1>Reset into New Passcode</h1>
 
 Hi Fellow, I heard you lost your password. If you really want to reset it, click the link below.
